cc50f7048fcb806f314518ee26dd27e58ab8a19d
GitHub Action <action@github.com>
2024-11-24 18:34:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/suhatanriverdi/autosaveontyping/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/suhatanriverdi/autosaveontyping/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.suhatanriverdi.autosaveontyping.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.suhatanriverdi.autosaveontyping.MyBundle
+import com.github.suhatanriverdi.autosaveontyping.services.MyProjectService
 import javax.swing.JButton
 
 
